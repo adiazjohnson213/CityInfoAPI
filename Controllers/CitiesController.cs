a71205df78ec4c193ad9c96ee096aa3a544abe85
Arthur Diaz <adiazjohnson213@gmail.com>
2023-08-28 17:53:23 -0500

    Adding controllers

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using CityInfoAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+
+            if (cityToReturn == null)
+                return NotFound();
+
+            return Ok(cityToReturn);
+        }
+    }
+}
---
+++ Controllers/PointsOfInterestController.cs
@@ -0,0 +1,38 @@
+﻿using CityInfoAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [Route("api/cities/{cityId}/[controller]")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+
+            if(city == null)
+                return NotFound();
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<PointOfInterestDto> GetPointsOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+
+            if (city == null)
+                return NotFound();
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(x => x.Id == pointOfInterestId);
+
+            if (pointOfInterest == null)
+                return NotFound();
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
